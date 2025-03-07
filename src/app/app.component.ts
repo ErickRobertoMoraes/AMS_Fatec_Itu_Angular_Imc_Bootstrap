b4fc68ac1609284c8f6522832a18b23022af6010
ErickRobertoMoraes <erick.roberto.moraes@outlook.com>
2025-03-07 14:56:25 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'AMS_Fatec_Itu_Angular_Imc_Bootstrap';
+}
